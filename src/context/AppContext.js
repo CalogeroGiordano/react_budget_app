33f0e6b0b897d4160fa218604aeaaadd11145e54
Calogero Giordano <93496084+CalogeroGiordano@users.noreply.github.com>
2023-10-01 17:32:00 -0400

    Completed project

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -85,8 +85,9 @@ const initialState = {
         { id: "Sales", name: 'Sales', cost: 70 },
         { id: "Human Resource", name: 'Human Resource', cost: 40 },
         { id: "IT", name: 'IT', cost: 500 },
+        { id: "Admin", name: 'Admin', cost: 300}
     ],
-    currency: '£'
+    currency: '$'
 };
 
 // 2. Creates the context this is the thing our components import and use to get the state
